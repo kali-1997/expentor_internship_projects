803de900de8de140aed7edd32aff8d3ecaa720a0
Kalaam <29440904+kali-1997@users.noreply.github.com>
2026-02-04 21:38:51 +0530

    Create sales_db and sales table in schema.sql

--- internship/sales_etl/schema.sql
+++ internship/sales_etl/schema.sql
@@ -1,1 +1,17 @@
 
+CREATE DATABASE IF NOT EXISTS sales_db;
+USE sales_db;
+
+CREATE TABLE IF NOT EXISTS sales (
+    order_id VARCHAR(20),
+    country VARCHAR(50),
+    category VARCHAR(50),
+    device_type VARCHAR(20),
+    customer_name VARCHAR(100),
+    sales_manager VARCHAR(100),
+    sales_rep VARCHAR(100),
+    estimate_order_val FLOAT,
+    cost FLOAT,
+    profit FLOAT,
+    order_date DATE
+);
