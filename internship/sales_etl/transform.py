c64a4a837806a152b8934bb4a9547d084b5920f4
Kalaam <29440904+kali-1997@users.noreply.github.com>
2026-02-04 21:35:20 +0530

    Update transform.py

--- internship/sales_etl/transform.py
+++ internship/sales_etl/transform.py
@@ -1,1 +1,23 @@
 
+import pandas as pd
+
+def transform_data(df):
+    # Clean numeric columns to remove any commas
+      df["estimate_order_val"] = df["estimate_order_val"].str.replace(",", "").astype(float)
+    df["cost"] = df["cost"].astype(float)
+    # datE convrsion
+    df["date"] = pd.to_datetime(df["date"])
+
+    # n/a removing
+      df["device_type"] = df["device_type"].fillna("Unknown")
+    # Remove duplicates
+    df = df.drop_duplicates()
+    # Create profit column
+      df["profit"] = df["estimate_order_val"] - df["cost"]
+      print("Data transformed")
+    return df
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/sales_data.csv")
+    df_clean = transform_data(df)
+    print(df_clean.head())
