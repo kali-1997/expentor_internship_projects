25566ea2ffbba51e9b505ffc687703f5f4ca7c99
Kalaam <29440904+kali-1997@users.noreply.github.com>
2026-02-04 21:31:20 +0530

    Implement data extraction from CSV file
    Added a function to extract data from a CSV file using pandas.

--- internship/sales_etl/extract.py
+++ internship/sales_etl/extract.py
@@ -1,1 +1,11 @@
 
+import pandas as pd
+
+def extract_data(path):
+    df = pd.read_csv(r"Desktop/files")
+    print("Data extraCted successfully")
+    return df
+
+if __name__ == "__main__":
+    df = extract_data("data/sales_data.csv")
+    print(df.head())
